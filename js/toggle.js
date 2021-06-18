6a0ca5206e1b80fd79a3e081a91afd8d9a1dad54
Karetnikova Valeriya <valeriya.karetnikova@gmail.com>
2021-06-18 10:52:57 -0600

    new mobile menu

--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,10 @@
 //Toggle navigation
 function toggleNavigation() {
-    document.querySelector('.hamburger-btn').addEventListener('click', () => {
+    document.querySelector('.menu-toggle').addEventListener('click', () => {
+        console.log("hello");
         document.querySelector('nav').classList.toggle('show-nav');
+        document.querySelector('.menu-toggle').classList.toggle('open');
+        document.body.classList.toggle('lock');
     });
 
     const menuLinks = document.querySelectorAll('.menu-links li a');
@@ -9,6 +12,8 @@ function toggleNavigation() {
         link.addEventListener('click', ()=> {
             if (document.querySelector('nav').classList.contains('show-nav')){
                 document.querySelector('nav').classList.remove('show-nav');
+                document.querySelector('.menu-toggle').classList.remove('open');
+                document.body.classList.remove('lock');
             }
         })
     })
